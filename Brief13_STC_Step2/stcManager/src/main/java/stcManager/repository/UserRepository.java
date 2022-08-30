6fb0ef32d9a56db4300b02cbfe512a3ee2378d9f
AYOUB OUDOR <oudour2000@gmail.com>
2022-08-30 10:15:02 +0200

    BR 13

---
+++ Brief13_STC_Step2/stcManager/src/main/java/stcManager/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package stcManager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import stcManager.security.AppUser;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long>{
+	
+		AppUser findByUsername(String username);
+}
